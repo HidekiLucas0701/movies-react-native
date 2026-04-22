be39b8c7d19ecad51cef494fafb37d0974167340
Lucas Hideki <hideki.lucas0701@gmail.com>
2026-04-22 20:01:15 -0300

    feat(wip): stack with button on top right

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -18,7 +18,19 @@ export default function App() {
           component={MainTabs}
           options={{headerShown: false}}
         />
-        
+        <Stack.Screen
+          name='MovieDetailsScreen'
+          component={MovieDetailsScreen}
+          options={({ navigation }) => ({
+            headerBackButtonDisplayMode: "minimal",
+            headerRight: () => (
+              <Button
+                title="Editar"
+                onPress={() => navigation.navigate("MovieFormScreen")}
+              />
+            )
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
